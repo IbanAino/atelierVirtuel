799fd994c95ad2fab89d0b4fde3274fe0e42870e
IbanAino <iban@ainolabs.com>
2017-01-19 16:52:31 +0100

    cimmit test 01

---
+++ AppleSelection.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleSelection : MonoBehaviour {
+
+    public Material appleOutlineMaterial;
+    public Material appleMaterial;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("handler"))
+        {
+            this.GetComponentInChildren<Renderer>().material = appleOutlineMaterial;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("handler"))
+        {
+            this.GetComponentInChildren<Renderer>().material = appleMaterial;
+        }
+    }
+}
