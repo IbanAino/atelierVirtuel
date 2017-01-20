1a88a4e0949ff136b88d20bf72ba7377127c2bf4
IbanAino <iban@ainolabs.com>
2017-01-20 14:23:59 +0100

    IA des avatars (en cours de développement)

--- BulleAvatar01.cs
+++ BulleAvatar01.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class BulleAvatar01 : MonoBehaviour {
 
-    public Animator anim;
+    private Animator anim;
     public bool avatarMovment;
     public GameObject avatar;
+
+    public int programState;
+
+    //variables for the avatar rotation
+    public bool rotate = false;
+    public Transform initialRotation;
+    public Transform secondRotation;
 
     //variables for the avatar movment
     public Vector3 startPosition;
@@ -22,21 +29,59 @@ public class BulleAvatar01 : MonoBehaviour {
         anim = avatar.GetComponent<Animator>();
 
         //for the avatar movment
-        speed = 0.5F;
-        startPosition = this.transform.position;
-        endPosition = this.transform.position + new Vector3(-10, 0, 0);
+        speed = 0.7F;
+        startPosition = avatar.transform.position;
+        endPosition = avatar.transform.position + new Vector3(-5, 0, 0);
         startTime = Time.time;
         journeyLength = Vector3.Distance(startPosition, endPosition);
     }
 
     void Update()
     {
-        if(avatarMovment == true)
+        if(programState == 1) //aller
         {
-            //GameObject.FindWithTag("Avatar01").transform.Translate(Vector3.forward * Time.deltaTime);
-            float distCovered = Time.time - startTime;
-            float fracJourney = (distCovered / journeyLength) * speed;
-            avatar.transform.position = Vector3.Lerp(startPosition, endPosition, fracJourney);
+
+            if (rotate == true)
+            {
+
+            }
+            else
+            {
+                float distCovered = Time.time - startTime;
+                float fracJourney = (distCovered / journeyLength) * speed;
+                avatar.transform.position = Vector3.Lerp(startPosition, endPosition, fracJourney);
+
+                if (avatar.transform.position == endPosition)
+                {
+                    anim.SetBool("idle2ToWalk", false);
+                    anim.SetBool("walkToIdle2", true);
+                    programState = 3;
+                }
+            }
+        }
+        else if(programState == 2) //retour
+        {
+            if(rotate == true)
+            {
+
+            }
+            else
+            {
+                float distCovered = Time.time - startTime;
+                float fracJourney = (distCovered / journeyLength) * speed;
+                avatar.transform.position = Vector3.Lerp(endPosition, startPosition, fracJourney);
+
+                if (avatar.transform.position == startPosition)
+                {
+                    anim.SetBool("idle2ToWalk", false);
+                    anim.SetBool("walkToIdle2", true);
+                    programState = 3;
+                }
+            }
+        }
+        else //repos
+        {
+
         }
     }
 
@@ -44,12 +89,29 @@ public class BulleAvatar01 : MonoBehaviour {
     {      
         if (other.gameObject.tag == "Player")
         {
-            //launch the animation
-            anim.SetBool("idle2ToWalk", true);
-            //move the avatar
-            avatarMovment = true;
+            programState = 1; //aller
 
-            Debug.Log("Bubble tuched");
+            anim.SetBool("walkToIdle2", false);
+            anim.SetBool("idle2ToWalk", true);
+
+            startTime = Time.time;
+
+            endPosition = startPosition + new Vector3(-5, 0, 0);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            programState = 2; //retour
+
+            anim.SetBool("walkToIdle2", false);
+            anim.SetBool("idle2ToWalk", true);
+
+            startTime = Time.time;
+
+            endPosition = avatar.transform.position;
         }
     }
 }
